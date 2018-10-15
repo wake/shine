73ba91149f63cdb3273ec8fc55e595ab3f0e7d58
l <wake.gs@gmail.com>
2018-10-16 01:47:11 +0800

    Add database config.

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+
+
+  /**
+   *
+   * DB config
+   *
+   */
+
+  /**
+   *
+   * Paris & Idiorm
+   *
+   */
+  ORM::configure ('mysql:host='. $_ENV['DB_HOST'] .';dbname='. $_ENV['DB_NAME']);
+  ORM::configure ('username', $_ENV['DB_USER']);
+  ORM::configure ('password', $_ENV['DB_PASS']);
+
+  ORM::configure ('logging',  $_ENV['DB_LOGGING']);
+  ORM::configure ('caching',  $_ENV['DB_CACHING']);
+  ORM::configure ('caching_auto_clear', $_ENV['DB_CACHING_AUTO_CLEAR']);
+
+  ORM::configure ('driver_options', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '. $_ENV['DB_CHARSET']));
